91581d06708709d508f145ab49d73309ee5aaa98
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2024-03-17 11:23:45 +0530

    Added geenting compnnt unit test

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting', () => {
@@ -11,5 +12,24 @@ describe('Greeting', () => {
     // Assert
     const h2El = screen.getByText('Hello World', { exact: false });
     expect(h2El).toBeInTheDocument();
+  });
+
+  test('Button was not clicked', () => {
+    render(<Greeting />);
+    const pEl = screen.getByText('good to see you', { exact: false });
+    expect(pEl).toBeInTheDocument();
+  });
+
+  test('Button was clicked', () => {
+    // arrange
+    render(<Greeting />);
+
+    // act
+    const buttonEl = screen.getByRole('button');
+    userEvent.click(buttonEl);
+
+    // assert
+    const pEl = screen.getByText('Changed', { exact: false });
+    expect(pEl).toBeInTheDocument();
   });
 });
