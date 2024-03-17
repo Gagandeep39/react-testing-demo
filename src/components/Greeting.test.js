53d56102e9134c2b198b9a3eeb39d571db6f4fcb
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2024-03-17 11:14:25 +0530

    Switched to  a test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('Whether we have Hello World', () => {
-  // Arrange
-  render(<Greeting />);
+describe('Greeting', () => {
+  test('Whether we have Hello World', () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act - Nothing
+    // Act - Nothing
 
-  // Assert
-  const h2El = screen.getByText('Hello World', { exact: false });
-  expect(h2El).toBeInTheDocument();
+    // Assert
+    const h2El = screen.getByText('Hello World', { exact: false });
+    expect(h2El).toBeInTheDocument();
+  });
 });
